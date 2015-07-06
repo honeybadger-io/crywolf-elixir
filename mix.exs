265f0ee0a78f7c7339098b26f54695a2580c780a
Joshua Wood <josh@hintmedia.com>
2015-07-06 16:15:10 -0700

    Add honeybadger app to mix.exs.

--- mix.exs
+++ mix.exs
@@ -18,7 +18,7 @@ defmodule Crywolf.Mixfile do
   def application do
     [mod: {Crywolf, []},
      applications: [:phoenix, :phoenix_html, :cowboy, :logger,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :honeybadger]]
   end
 
   # Specifies which paths to compile per environment
