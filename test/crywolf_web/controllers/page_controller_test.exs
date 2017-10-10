098e574eb579bdf9317d3de41af1524d0bba1d7a
Joshua Wood <josh@joshuawood.net>
2017-10-10 10:18:54 -0700

    Fix test.

--- test/crywolf_web/controllers/page_controller_test.exs
+++ test/crywolf_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CrywolfWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Hello Crywolf!"
   end
 end
